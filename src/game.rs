fc8346833151181c2b7b193a2d6027819ba630f3
Juan Medina <jamedina@gmail.com>
2023-01-14 11:17:21 +0000

    adding UI scaling

--- src/game.rs
+++ src/game.rs
@@ -22,24 +22,83 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ***/
 
 use crate::hello;
-use bevy::prelude::*;
+use bevy::{
+    log::LogPlugin,
+    prelude::*,
+    text::TextSettings,
+    window::{WindowResizeConstraints, WindowResized},
+    winit::WinitSettings,
+};
 
 const TITLE: &str = "Dice Master!";
+const LOG_FILTER: &str = "wgpu=error,dice_master=debug";
+const DESIGN_RESOLUTION: Vec2 = Vec2::new(1920., 1080.);
 
 pub fn run() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            window: WindowDescriptor {
-                title: TITLE.into(),
-                ..Default::default()
-            },
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    window: WindowDescriptor {
+                        title: TITLE.into(),
+                        width: DESIGN_RESOLUTION.x,
+                        height: DESIGN_RESOLUTION.y,
+                        resize_constraints: WindowResizeConstraints {
+                            min_width: DESIGN_RESOLUTION.x / 2.,
+                            min_height: DESIGN_RESOLUTION.y / 2.,
+                            ..default()
+                        },
+                        ..Default::default()
+                    },
+                    ..default()
+                })
+                .set(LogPlugin {
+                    filter: LOG_FILTER.into(),
+                    level: bevy::log::Level::INFO,
+                }),
+        )
+        .insert_resource(TextSettings {
+            allow_dynamic_font_size: true,
             ..default()
-        }))
+        })
+        .insert_resource(WinitSettings::desktop_app())
+        .add_system(bevy::window::close_on_esc)
         .add_startup_system(setup)
         .add_startup_system(hello::hello_world)
+        .add_system(scale_ui)
+        .add_system(toggle_full_screen_on_alt_enter)
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+fn scale_ui(resize_event: Res<Events<WindowResized>>, mut ui_scale: ResMut<UiScale>) {
+    let mut reader = resize_event.get_reader();
+    for e in reader.iter(&resize_event) {
+        let scale_x = e.width / DESIGN_RESOLUTION.x;
+        let scale_y = e.height / DESIGN_RESOLUTION.y;
+        let scale = (scale_x * scale_y).sqrt();
+        ui_scale.scale = scale as f64;
+    }
+}
+
+fn toggle_full_screen_on_alt_enter(input: Res<Input<KeyCode>>, windows: ResMut<Windows>) {
+    if (input.pressed(KeyCode::LAlt) || input.pressed(KeyCode::RAlt))
+        && input.just_pressed(KeyCode::Return)
+    {
+        toggle_full_screen(windows);
+    }
+}
+
+fn toggle_full_screen(mut windows: ResMut<Windows>) {
+    let window = windows
+        .get_primary_mut()
+        .expect("we should have a primary window");
+    if window.mode() == WindowMode::Windowed {
+        window.set_mode(WindowMode::BorderlessFullscreen);
+    } else {
+        window.set_mode(WindowMode::Windowed);
+    }
 }
